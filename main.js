18f4225fc62006d97d24d2efbdb16a9455357657
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-26 07:49:09 -0600

    fin

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const express = require('express'), app = express(),
-homecontroller = require("./controllers/homeController"),
+const express = require('express'), app = express();
+homecontroller = require("./controllers/homeController");
 errorController = require("./controllers/errorController");
+subscribersController = require("./controllers/subscribersController");
 
 layouts = require("express-ejs-layouts");
+mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine",{useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set("port",process.env.PORT||3000);
 
@@ -21,8 +25,13 @@ app.use(express.json());
 
 
 app.get("/courses", homecontroller.showCourses);
-app.get("/contact", homecontroller.showSignUp);
-app.post("/contact",homecontroller.postedSignUpForm);
+app.get("/subscribers", subscribersController.getAllSubscribers);
+app.get("/contact",subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
+
+
+//app.get("/contact", homecontroller.showSignUp);
+//app.post("/contact",homecontroller.postedSignUpForm);
 
 
 app.use(errorController.pageNotFoundError);
